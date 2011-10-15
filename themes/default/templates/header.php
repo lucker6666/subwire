04cf97bb9b9be9b4b2d740853f209d558da86071
Benjamin Kammerl <benjamin.kammerl@chamaeleon.de>
2011-10-15 22:52:19 +0200

    comment overview

--- themes/default/templates/header.php
+++ themes/default/templates/header.php
@@ -39,7 +39,7 @@ if(!strcasecmp(basename($_SERVER['SCRIPT_NAME']),basename(__FILE__))) die('Kwahe
 			if (count($__linklist) > 0) {
 				echo '<ul class="linklist">';
 				echo '<li><strong>' . LINKLIST_LABEL . ':</strong></li>';
-				
+
 				foreach ($__linklist as $label=>$url) {
 					echo '<li><a href="' . $url . '" target="_blank">' . $label . '</a></li>';
 				}
@@ -58,7 +58,27 @@ if(!strcasecmp(basename($_SERVER['SCRIPT_NAME']),basename(__FILE__))) die('Kwahe
             <a href="<?=rtrim(SITE_URL, '/')?>/manage_post.php?action=new&type=video" title="New Video Post"><img src="images/icons/video.png" width="32" height="32" alt="New Video Post"></a>
             <a href="<?=rtrim(SITE_URL, '/')?>/manage_post.php?action=new&type=audio" title="New Audio Post"><img src="images/icons/audio.png" width="32" height="32" alt="New Audio Post"></a>
             <a href="<?=rtrim(SITE_URL, '/')?>/manage_post.php?action=new&type=code" title="New Code Post"><img src="images/icons/code.png" width="32" height="32" alt="New Code Post"></a>
+
+			<div class="right">
+				<a href="#" title="Show last comments"><img src="images/icons/comments.png" width="32" height="32" alt="Show last
+				comments"></a>
+			</div>
         </nav>
+
+		<div class="lastComments">
+			<ul>
+				<? foreach(get_comments() as $c): ?>
+                    <li>
+                        <span style="color: #<?= user_color($c['user']); ?>;"><?= $c['user'] ?>:</span>
+                        <?= $c['contents'] ?>
+						<i>
+						(<?=Format::localDateTime($c['posted_on'],$format='d.m. - H:i',$offset=TZ,$daylight=USE_DST)?>)
+						</i>
+                    </li>
+                <? endforeach; ?>
+			</ul>
+		</div>
+
         <? if(isset($_SESSION['flash_notice'])): ?>
         <div id="flash_notice"><?=$_SESSION['flash_notice']?></div>
         <? unset($_SESSION['flash_notice']); ?>
