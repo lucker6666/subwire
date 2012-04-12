0078e001b0c9c38ca4c514165028f5dabd6e9314
benny <benny@itws.de>
2012-04-12 08:57:50 +0200

    devise

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,7 @@ BrainDump::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # URL
+  config.action_mailer.default_url_options = { :host => 'localhost:3001' }
 end
