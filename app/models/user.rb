a71c08dc668274c173bfba2b878ec92b3c2a7552
benny <benny@itws.de>
2012-04-19 22:38:12 +0200

    snapshot

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :validatable
 
-  attr_accessible :login, :is_admin, :name, :email, :password, :password_confirmation, :remember_me, :color
+  attr_accessible :login, :is_admin, :name, :email, :password, :password_confirmation,
+  	:remember_me, :color, :last_seen
 end
---
+++ db/migrate/20120419202442_notifications.rb
@@ -0,0 +1,14 @@
+class Notifictations < ActiveRecord::Migration
+  def change
+  	create_table :notifictations do |t|
+      t.string :type, :default => "article"
+      t.string :message
+      t.string :href
+      t.boolean :is_read
+
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
