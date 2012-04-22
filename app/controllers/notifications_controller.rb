a2ce1e9b8cdf28311067b0e003ea2478b61e29a7
benny <benny@itws.de>
2012-04-22 14:46:29 +0200

    notification routes and controller

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,16 @@
+class NotificationsController < ApplicationController
+	# GET /comments/1
+  def show
+  	if (params[:id])
+    	@notification = Notification.find(params[:id])
+			target = "/"
+
+    	if @notification && @notification.user == current_user
+ 				target = @notification.href
+    		@notification.destroy
+    	end
+    end
+
+   	redirect_to target
+  end
+end
