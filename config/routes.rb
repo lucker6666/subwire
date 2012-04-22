93e1b2155831cea58bec85e264c313d9eb6788e3
benny <benny@itws.de>
2012-04-22 14:46:29 +0200

    notification routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 BrainDump::Application.routes.draw do
+	# Devise
   devise_for :users
 
+  # Links
   resources :links
 
+  # Articles and comments
   resources :articles do
 	  resources :comments
 	end
 
+	# Notifications
+	resources :notifications, :only => [:show]
 
+
+	# Start page
   get "home/index"
   root :to => "articles#index"
 end
