a5fc3cdfd1eae0582f4c017b891d0298b5b5ecba
benny <benny@itws.de>
2012-05-03 10:00:06 +0200

    formatting

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,18 +1,18 @@
 class NotificationsController < ApplicationController
   before_filter :authenticate_user!
 
-	# GET /comments/1
-  def show
-  	if (params[:id])
-    	@notification = Notification.find(params[:id])
+	# GET /notifications/1
+	def show
+	  	if (params[:id])
+			notification = Notification.find(params[:id])
 			target = "/"
 
-    	if @notification && @notification.user == current_user
- 				target = @notification.href
-    		@notification.destroy
-    	end
-    end
+			if notification && notification.user == current_user
+				target = notification.href
+				notification.destroy
+			end
 
-   	redirect_to target
-  end
+	   		redirect_to target
+	  	end
+	 end
 end
