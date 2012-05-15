e7e74946a8e899a1acb809bfa06a9afb12c3b15b
Benjamin August <benji889@yahoo.de>
2012-05-16 01:17:30 +0200

    xhr notification update

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,16 @@
 class NotificationsController < ApplicationController
 	before_filter :authenticate_user!
 
+	# GET /notifications.json
+	def index
+		@notifications = Notification.find_all_by_user_id(current_user.id)
+
+		respond_to do |format|
+			#format.html # index.html.erb
+			format.json { render json: @notifications }
+		end
+	end
+
 	# GET /notifications/1
 	def show
 		if (params[:id])
