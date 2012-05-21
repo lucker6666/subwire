59349e606d7f43a19f48857ec2d4c0a0ce8008a3
benny <benny@itws.de>
2012-05-21 09:43:11 +0200

    availabilities added

--- db/migrate/20120521072815_multi_instances.rb
+++ db/migrate/20120521072815_multi_instances.rb
@@ -1,6 +1,6 @@
 class MultiInstances < ActiveRecord::Migration
   def change
-  	# instances table
+  	# instances table and the references
   	create_table :instances do |i|
       t.string 	:name, :null => false
       t.string 	:defaultLanguage, :null => false, :default => 'en'
@@ -16,17 +16,24 @@ class MultiInstances < ActiveRecord::Migration
     add_column :articles, :instance_id, :integer, :null => false
     add_index :articles, :instance_id
 
+    add_column :availabilities, :instance_id, :integer, :null => false
+    add_index :availabilities, :instance_id
+
+
     # relationships table
     create_table :relationships do |t|
       t.references 	:user, :null => false
       t.references 	:instance, :null => false
       t.boolean			:admin, :null => false, :default => false
     end
+
     add_index :relationships, :instance_id
     add_index :relationships, :user_id
 
-    # article types
+
+    # article type column
     add_column :articles, :type, :string, :null => false, :default => 'article'
+
 
     # user language and avatar columns
     add_column :users, :lang, :string, :null => false, :default => 'en'
