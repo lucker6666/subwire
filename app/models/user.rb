0499cb8f658a7e1ec9563a44f5f19063ce60e435
benny <benny@itws.de>
2012-05-21 11:29:05 +0200

    authentication changed from login to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	devise :database_authenticatable, :rememberable, :validatable
 
-	attr_accessible :login, :is_admin, :name, :email, :password, :password_confirmation,
+	attr_accessible :is_admin, :name, :email, :password, :password_confirmation,
 		:remember_me, :color, :last_seen, :relationship
 
 	has_many :comments
