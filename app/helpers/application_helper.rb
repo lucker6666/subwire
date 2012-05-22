0745c6eff82ce48e5c71f33441b1b7cb66a1b8d2
benny <benny@itws.de>
2012-05-22 17:23:48 +0200

    finished the check_admin method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
 	def colored_name(user)
 		"<strong style='color: \##{user.color};'>#{user.name}</strong>".html_safe
 	end
+
+	# Return true if the current user is admin of the current instance
+	def is_admin_of_instance?
+		rel = Relationship.where(:user_id => current_user.id, :instance_id => current_instance.id).first
+		rel.admin?
+	end
 end
