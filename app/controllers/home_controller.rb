911041cd3d5e44be089d6ed86241fdd69bc3ce9f
benny <benny@itws.de>
2012-05-23 17:29:42 +0200

    If there is already a current_instance, redirect to that instance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,17 @@ class HomeController < ApplicationController
 
 	# GET /
 	def index
-		@instances = Instance.find_all_by_user(current_user)
-
-		if @instances.length > 1
-			redirect_to instances_path
+		if current_instance
+				redirect_to articles_path
 		else
-			set_current_instance @instances.first
-			redirect_to articles_path
+			@instances = Instance.find_all_by_user(current_user)
+
+			if @instances.length > 1
+				redirect_to instances_path
+			else
+				set_current_instance @instances.first
+				redirect_to articles_path
+			end
 		end
 	end
 end
