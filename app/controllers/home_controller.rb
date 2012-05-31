0a20465ca9900cde15acc9b46c936eeb9f81ec9b
benny <benny@itws.de>
2012-05-31 22:01:10 +0200

    bugfix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,9 @@ class HomeController < ApplicationController
 	# GET /
 	def index
 		if current_instance
-				redirect_to articles_path
+			redirect_to articles_path
 		else
-			@instances = Instance.find_all_by_user(current_user)
-
-			if @instances.length > 1
-				redirect_to instances_path
-			else
-				set_current_instance @instances.first
-				redirect_to articles_path
-			end
+			redirect_to instances_path
 		end
 	end
 end
