2f4b28b00b7010172db6167dd315e0049397760e
benny <benny@itws.de>
2012-06-12 21:59:25 +0200

    formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,37 @@
 BrainDump::Application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
+	mount Ckeditor::Engine => '/ckeditor'
 
 
 	# Devise
-  devise_for :users
+	devise_for :users
 
-  # Unset current instance
-  get "instances/unset", :to => "instances#unset"
+	# Unset current instance
+	get "instances/unset", :to => "instances#unset"
 
-  # Instances
+	# Instances
 	resources :instances
 
 	# Users
-  resources :users, :only => [:index, :show, :edit, :update, :destroy]
+	resources :users, :only => [:index, :show, :edit, :update, :destroy]
 
-  # Relationships
-  resources :relationships, :only => [:index, :create, :new, :edit, :update, :destroy]
+	# Relationships
+	resources :relationships, :only => [:index, :create, :new, :edit, :update, :destroy]
 
-  # Links
-  resources :links
+	# Links
+	resources :links
 
-  # Articles and comments
-  resources :articles do
-	  resources :comments, :only => [:create, :update, :destroy]
+	# Articles and comments
+	resources :articles do
+		resources :comments, :only => [:create, :update, :destroy]
 	end
 
 	# Notifications
 	resources :notifications, :only => [:index, :show]
 
 	# Availability Tool
-  post "availability", :to => "availabilities#set"
+	post "availability", :to => "availabilities#set"
 
 	# Start page
-  get "home/index"
-  root :to => "home#index"
+	get "home/index"
+	root :to => "home#index"
 end
