638ddf0b0631d711b0e7989b53e5632f1bcd9e8c
benny <benny@itws.de>
2012-06-12 14:18:45 +0200

    refactorings, optimizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::Base
 
 		# Sends a message to the user over the feedback system. Will use jGrowl in frontend.
 		def feedback(msg)
-			# Case 1: nil or empty string
+			# Case 1: nil or empty string or empty array
 			if flash[:alert].nil? || flash[:alert].to_s.strip.length == 0
 				flash[:alert] = [msg]
 				return
@@ -58,42 +58,15 @@ class ApplicationController < ActionController::Base
 			end
 		end
 
+
+		# Filters:
+
 		# Reloads the current instance config from database
 		def refresh_config
 			if current_instance
 				set_current_instance Instance.find(current_instance.id)
 			end
 		end
-
-		# Checks wether the current user is allowed to see the requested instance. If not,
-		# the user will be redirected to the instance overview.
-		def check_permissions
-			# Logged in? If not, redirect to login mask
-			if current_user
-				# Is there a instance choosen? If not, redirect to the instance overview
-				if current_instance
-					# If the user is a superadmin everything is ok
-					unless current_user.is_admin?
-						# Get the relationship between current_user and current_instance)
-						rs = current_rs
-
-						# If there is no relationship, the user is not allowed to see that instance.
-						# So redirect to instances overview. Otherwise everything is ok
-						unless rs
-							feedback t :application.no_permission_for_instance
-							redirect_to instances_path
-						end
-					end
-				else
-					redirect_to instances_path
-				end
-			else
-				redirect_to "/"
-			end
-		end
-
-
-		# Filters:
 
 		# Filter that returns true if the user has admin privileges. That's if the user is a superadmin
 		#  (is_admin flag of user) or the user is admin for current_instance.
@@ -127,6 +100,33 @@ class ApplicationController < ActionController::Base
 		def choose_instance!
 			unless current_instance
 				redirect_to instances_path
+			end
+		end
+
+		# Checks wether the current user is allowed to see the requested instance. If not,
+		# the user will be redirected to the instance overview.
+		def check_permissions
+			# Logged in? If not, redirect to login mask
+			if current_user
+				# Is there a instance choosen? If not, redirect to the instance overview
+				if current_instance
+					# If the user is a superadmin everything is ok
+					unless current_user.is_admin?
+						# Get the relationship between current_user and current_instance)
+						rs = current_rs
+
+						# If there is no relationship, the user is not allowed to see that instance.
+						# So redirect to instances overview. Otherwise everything is ok
+						unless rs
+							feedback t :application.no_permission_for_instance
+							redirect_to instances_path
+						end
+					end
+				else
+					redirect_to instances_path
+				end
+			else
+				redirect_to "/"
 			end
 		end
 end
