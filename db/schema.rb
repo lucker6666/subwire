92b3e3fdb2aa97514c5b6018f40c8115abd10f93
benny <benny@itws.de>
2012-06-14 17:21:34 +0200

    current schema ...

--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,9 @@ ActiveRecord::Schema.define(:version => 20120521072815) do
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.integer  "instance_id", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "instance_id", :default => 1, :null => false
   end
 
   add_index "articles", ["instance_id"], :name => "index_articles_on_instance_id"
@@ -28,9 +28,9 @@ ActiveRecord::Schema.define(:version => 20120521072815) do
     t.integer  "user_id"
     t.datetime "date"
     t.boolean  "value"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.integer  "instance_id", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "instance_id", :default => 1, :null => false
   end
 
   add_index "availabilities", ["instance_id"], :name => "index_availabilities_on_instance_id"
@@ -71,9 +71,9 @@ ActiveRecord::Schema.define(:version => 20120521072815) do
     t.string   "name"
     t.string   "href"
     t.string   "icon"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.integer  "instance_id", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "instance_id", :default => 1, :null => false
   end
 
   add_index "links", ["instance_id"], :name => "index_links_on_instance_id"
@@ -86,14 +86,14 @@ ActiveRecord::Schema.define(:version => 20120521072815) do
     t.integer  "user_id"
     t.datetime "created_at",                               :null => false
     t.datetime "updated_at",                               :null => false
-    t.integer  "instance_id",                              :null => false
+    t.integer  "instance_id",       :default => 1,         :null => false
   end
 
   add_index "notifications", ["instance_id"], :name => "index_notifications_on_instance_id"
 
   create_table "relationships", :force => true do |t|
-    t.integer "user_id",                       :null => false
-    t.integer "instance_id",                   :null => false
+    t.integer "user_id",     :default => 1,    :null => false
+    t.integer "instance_id", :default => 1,    :null => false
     t.boolean "admin",       :default => true, :null => false
   end
 
