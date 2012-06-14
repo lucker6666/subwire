5d6df6126ffd328d18287f5db9d929dc1b854cff
benny <benny@itws.de>
2012-06-14 15:16:00 +0200

    bugfix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,7 +12,7 @@
 # TODO: article_id, user_id not null?
 
 class Comment < ActiveRecord::Base
-	attr_accessible :content, article_id
+	attr_accessible :content, :article_id
 
 	belongs_to :article
 	belongs_to :user
