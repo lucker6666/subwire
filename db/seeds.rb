4ba19e8206f10793fb0040e2bfe456cac0343394
benny <benny@itws.de>
2012-06-20 16:45:22 +0200

    removed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,1 @@
-User.create(
-	:login => 'admin',
-	:name => 'Admin',
-	:email => 'admin@example.com',
-	:is_admin => true,
-	:password => 'admin',
-  :color => 'f92672'
-)
 
-Article.create(
-	:title => "Welcome to your new BrainDump!",
-	:content => "Hi, this is your new and fresh BrainDump installation. Have fun! :)",
-	:user => User.find(1)
-)
