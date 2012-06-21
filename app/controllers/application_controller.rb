604214ca001f9edb13d9fea3c96f729454a9e182
Benjamin August <benji889@yahoo.de>
2012-06-21 22:30:44 +0200

    no comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -78,9 +78,7 @@ class ApplicationController < ActionController::Base
 		# Reloads the current instance config from database
 		def refresh_config
 			if current_instance
-				if defined?(resource) && !resource.nil?
-					set_current_instance Instance.find(ressource.instance_id)
-				end
+				set_current_instance Instance.find(current_instance.id)
 			end
 		end
 
