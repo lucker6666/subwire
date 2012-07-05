d5cd07f7945285543b792700b1786ebc77e4e89a
benny <benny@itws.de>
2012-07-05 17:22:49 +0200

    added indexes

--- db/migrate/20120703185458_recoverable.rb
+++ db/migrate/20120703185458_recoverable.rb
@@ -3,5 +3,8 @@ class Recoverable < ActiveRecord::Migration
 	  change_table(:users) do |t|
 	    t.confirmable
 	  end
+
+  	add_index :users, :reset_password_token, :unique => true
+  	add_index :users, :authentication_token, :unique => true
 	end
 end
