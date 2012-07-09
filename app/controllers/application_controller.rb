2250fc7d3daff92c8650ae5e7565db778cf54782
benny <benny@itws.de>
2012-07-09 19:31:37 +0200

    formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -174,8 +174,7 @@ class ApplicationController < ActionController::Base
 
 		#Custom errorpages
 		def render_optional_error_file(status_code)
-	      status = interpret_status(status_code)
-	      render :template => "/errors/#{status[0,3]}.html.erb", :status => status, :layout => 'application.html.erb'
-	    end
-
+      status = interpret_status(status_code)
+			render :template => "/errors/#{status[0,3]}.html.erb", :status => status, :layout => 'application.html.erb'
+		end
 end
