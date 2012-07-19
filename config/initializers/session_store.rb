e25568ff5dd41e0027d00e337ce21b57ebe778a2
benny <benny@itws.de>
2012-07-19 19:06:43 +0200

    session_stor domain removed

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Subwire::Application.config.session_store :cookie_store, key: '_Subwire_session', :domain => "subwire.net"
+Subwire::Application.config.session_store :cookie_store, key: '_Subwire_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
