d5961f6175cddd8eacb0f2085b1b7a0f5f7f0bc7
benny <benny@itws.de>
2012-07-26 10:03:09 +0200

    specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe HomeController do
-
+	# Get / unauthorized
   describe "GET 'index'" do
     it "should be successful" do
       get 'index'
-      response.should be_success
+    	response.should redirect_to(new_user_session_url)
     end
   end
-
 end
