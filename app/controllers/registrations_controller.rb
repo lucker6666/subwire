4f9334b784a4c3dff6e61191d3141ed381f2aacb
benny <benny@itws.de>
2012-08-02 21:21:54 +0200

    revert closed-beta

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,12 +4,30 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def new
+  	super
   end
 
   def create
-  	render :new
+  	build_resource
+
+  	#Exist deleted user with this mailadress?
+  	@user = User.where(:email => resource.email, :is_deleted => true)
+
+  	if(@user.length > 0)
+  		@user.each do |u|
+				u.update_attributes(params[:user])
+				u.is_deleted = false
+				u.save
+				sign_in(u)
+			end
+
+			redirect_to "/instances"
+		else
+			super
+  	end
   end
 
   def edit
+  	super
   end
 end
