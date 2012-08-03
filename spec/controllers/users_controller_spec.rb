0f533c8d8473e13b69b17d507524629734203ff0
benny <benny@itws.de>
2012-08-03 11:09:20 +0200

    factories and fixed spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,24 +1,21 @@
 require 'spec_helper'
 
 describe UsersController do
-
   before (:each) do
-    @user = FactoryGirl.create(:user)
-    sign_in @user
+    @rel = FactoryGirl.create(:user2_with_instance)
+    sign_in @rel.user
+    set_current_instance @rel.instance
   end
 
   describe "GET 'show'" do
-    
     it "should be successful" do
-      get :show, :id => @user.id
+      get :show, :id => @rel.user.id
       response.should be_success
     end
-    
-    it "should find the right user" do
-      get :show, :id => @user.id
-      assigns(:user).should == @user
-    end
-    
-  end
 
+    it "should find the right user" do
+      get :show, :id => @rel.user.id
+      assigns(:user).should == @rel.user
+    end
+  end
 end
--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    name 'Test User'
-    email 'example@example.com'
-    password 'testpw'
-    password_confirmation 'testpw'
-    confirmed_at Time.now
-  end
-end
