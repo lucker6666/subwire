1312de2e854d92ce445c22ee981d57ea69ca2bc2
benny <benny@itws.de>
2012-08-27 08:31:01 +0200

    helpers and factories

---
+++ spec/support/rspec_helper.rb
@@ -0,0 +1,11 @@
+module Subwire
+	module RSpecHelper
+		def login(user)
+			post login_path, :login => user.email, :password => user.password
+		end
+
+		def current_user
+			subject.send(:current_user)
+		end
+	end
+end
