673cb0049f13dc7325f900710c378c56fafe8ddc
Benjamin August <benji889@yahoo.de>
2012-09-12 23:42:13 +0200

    Mark all Notifications as read

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -37,4 +37,29 @@ class NotificationsController < ApplicationController
 			redirect_to target
 		end
 	 end
+
+	# DELETE /notifications/1.json
+	def destroy
+
+		#Mark all as read
+		notifications = Notification.where(
+				:user_id => current_user.id,
+				:instance_id => current_instance.id,
+				:is_read => false
+			)
+
+		notifications.each do |notification|
+			notification.read!
+		end
+
+		#Return the last 5 Notifications
+		@notifications = Notification.order("is_read").order("created_at DESC").limit(5).where(
+			:user_id => current_user.id,
+			:instance_id => current_instance.id
+		)
+
+		respond_to do |format|
+			format.json { render json: @notifications, :methods => [:message, :avatar_path] }
+		end
+	end
 end
