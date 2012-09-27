9dfbebe3863841a6b88af47aa0604c47fcc230e7
benny <benny@itws.de>
2012-09-27 17:52:30 +0200

    The set method now does nothing unless is logged in

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,24 +1,26 @@
 class AvailabilitiesController < ApplicationController
   # POST /availabilities/set
   def set
-    paramSet = {
-      date: params[:date],
-      value: params[:value]
-    }
+    if current_user && current_channel && (current_user.is_admin? || current_rs)
+      paramSet = {
+        date: params[:date],
+        value: params[:value]
+      }
 
-    availability = Availability.where(
-      user_id: current_user.id,
-      channel_id: current_channel.id,
-      date: params[:date]
-    ).first
+      availability = Availability.where(
+        user_id: current_user.id,
+        channel_id: current_channel.id,
+        date: params[:date]
+      ).first
 
-    if availability.nil?
-      availability = Availability.new(paramSet)
-      availability.user = current_user
-      availability.channel = current_channel
-      availability.save
-    else
-      availability.update_attributes(paramSet)
+      if availability.nil?
+        availability = Availability.new(paramSet)
+        availability.user = current_user
+        availability.channel = current_channel
+        availability.save
+      else
+        availability.update_attributes(paramSet)
+      end
     end
 
     head :no_content
