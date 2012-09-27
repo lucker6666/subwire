da35f37bb7243edb15cdf32795d928382e3a63a7
benny <benny@itws.de>
2012-09-27 17:53:37 +0200

    renamed

---
+++ spec/controllers/availabilities_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe AvailabilitiesController do
+  before (:each) do
+    @rel = FactoryGirl.create(:user1_with_channel)
+  end
+
+  describe "GET 'set'" do
+    context "while not authorized" do
+      it "should not set an availability" do
+        get :set, value: 1, date: Date.today
+        Availability.all.any?.should be false
+      end
+    end
+
+    context "while authorized" do
+      before do
+        # Log in first
+        sign_in @rel.user
+      end
+
+      it "should set an availability for that date" do
+        get :set, value: 1, date: Date.today
+        Availability.where(value: 1, date: Date.today).any?.should be false
+
+        get :set, value: 0, date: Date.today - 1
+        Availability.where(value: 0, date: Date.today - 1).any?.should be false
+      end
+    end
+  end
+end
