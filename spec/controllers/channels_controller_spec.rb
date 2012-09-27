e36c47c71932d2237ecc87fdaefc2a57891b4d02
benny <benny@itws.de>
2012-09-27 18:41:48 +0200

    channel spec

--- spec/controllers/channels_controller_spec.rb
+++ spec/controllers/channels_controller_spec.rb
@@ -58,10 +58,6 @@ describe ChannelsController do
         get :show, id: @rel.channel.id
       end
 
-      it "should be successful" do
-        response.should be_success
-      end
-
       it "should set the current channel" do
         current_channel.should == @rel.channel
       end
@@ -84,7 +80,64 @@ describe ChannelsController do
         it "should be successful" do
           response.should be_success
         end
+
+        it "should assign a @channel variable" do
+          #assigns[:channel].should == Channel.new
+          # TODO doesn't work !?
+        end
+
+        it "should render the 'new' view in the login layout" do
+          response.should render_template(['layouts/login', 'channels/new'])
+        end
       end
     end
+
+    context "get 'edit'" do
+      context "while has no superadmin privileges and not assigned to that channel" do
+        # TODO
+      end
+
+      context "while has superadmin privileges but not assigned to that channel" do
+        # TODO
+      end
+
+      context "while has no superadmin privileges but assigend to that channel" do
+        before do
+          get :edit, id: @rel.channel.id
+        end
+
+        it "should be successful" do
+          response.should be_success
+        end
+
+        it "should assign the @channel variable which should be the channel with ID 1" do
+          assigns[:channel].id.should == @rel.channel.id
+        end
+
+        it "should render the 'edit' view in the login layout" do
+          response.should render_template(['layouts/login', 'channels/edit'])
+        end
+      end
+    end
+
+    # TODO: POST /channels
+    # TODO: PUT /channels/1
+    # TODO: DELETE /channels/1
+
+    context "get 'unset'" do
+      before do
+        get :unset
+      end
+
+      it "should unset the current channel" do
+        current_channel.should be nil
+      end
+
+      it "should redirect to channel overview" do
+        response.should redirect_to(channels_path)
+      end
+    end
+
+    # TODO: GET /channels/all
   end
 end
