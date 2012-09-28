d0169a591352a466bb9f1a7c9b06d6f6aa14684d
sebastianbrozda <sebastian.brozda@gmail.com>
2012-09-28 11:02:31 +0200

    Feature #353 - Online status update via ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -163,4 +163,8 @@ class UsersController < ApplicationController
       render :finish, layout: "login"
     end
   end
+
+  def ajax_load_user_box
+    render :partial => 'layouts/shared/user_box', :locals => {:sidebar_users => @sidebar_users}
+  end
 end
