07f2f24a578928cc3dfc2579464106a320503212
sebastianbrozda <sebastian.brozda@gmail.com>
2012-10-25 15:35:16 +0200

    Tag article as editable #318

--- features/step_definitions/article_steps.rb
+++ features/step_definitions/article_steps.rb
@@ -1,3 +1,23 @@
 And /I follow to the article page/ do
   visit '/articles/1/edit'
 end
+
+Then /I add new article/ do
+  steps %{
+  And I am on the adding article form
+  When I fill in "article_title" with "New article"
+  And I fill in "article_content" with "New article content"
+  And I check "article_is_editable"
+  And I press "Create Article"
+  }
+end
+
+
+Then /I update article title/ do
+  steps %{
+  And I am on the article list
+  When I follow to the article page
+  Then I fill in "article_title" with "New title"
+  And I press "Update Article"
+  }
+end
