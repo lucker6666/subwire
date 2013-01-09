8bb3401d970f86167bbeac5226ca3311448fc9fb
benny <benny@itws.de>
2013-01-09 09:58:12 +0100

    Bugfix: password_confirmation not longer required

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,7 +24,6 @@ describe User do
       should validate_presence_of(:timezone)
       should validate_presence_of(:email)
       should validate_presence_of(:password)
-      should validate_presence_of(:password_confirmation)
 
       should ensure_length_of(:password).is_at_least(5).is_at_most(32)
 
