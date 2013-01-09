1b2aeae1580df588bd0ca5dacd0129dcf728fe1b
Benjamin August <benji889@yahoo.de>
2013-01-09 22:17:55 +0100

    bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,7 +60,7 @@ class User < ActiveRecord::Base
   validates :lang, :timezone, :email, :password, presence: true
 
   # Make sure name contains no invalid chars and length is between 3 and 30
-  validates :name, format: { with: /[a-zA-Z0-9\-_. ]+/ },
+  validates :name, format: { with: /^[a-zA-Z0-9\-_. ]+$/ },
     length: { minimum: 3, maximum: 30 }
 
   # Make sure email looks like an email adress
