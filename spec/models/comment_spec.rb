ac01f330ffc649b8be18714710a994ad99dca8de
benny <benny@itws.de>
2013-01-31 16:51:55 +0100

    More coverage

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -13,4 +13,14 @@ describe Comment do
   describe "Validation" do
     it { should validate_presence_of(:content) }
   end
+
+  describe :newest_comments do
+    before do
+      @article = FactoryGirl.create(:channel).articles.first
+    end
+
+    it "sould return all comments of that article" do
+      @article.newest_comments.should eq(Comment.newest.find_all_by_article_id(@article.id))
+    end
+  end
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -15,12 +15,34 @@ describe Relationship do
     it { should respond_to(:email) }
   end
 
-  it "should return a relationship by user and channel" do
-    channel = FactoryGirl.create(:channel)
-    rel1 = channel.relationships.first
+  describe :find_by_channel_and_user do
+    it "should return a relationship by user and channel" do
+      channel = FactoryGirl.create(:channel)
+      rel1 = channel.relationships.first
 
-    rel2 = Relationship.find_by_channel_and_user(rel1.channel, rel1.user)
+      rel2 = Relationship.find_by_channel_and_user(rel1.channel, rel1.user)
 
-    rel2.should eq(rel1)
+      rel2.should eq(rel1)
+    end
+  end
+
+  describe :email do
+    it "should return the emailadress of the associated user" do
+      channel = FactoryGirl.create(:channel)
+      rel = channel.relationships.first
+
+      rel.email.should eq(rel.user.email)
+    end
+  end
+
+  describe :find_all_by_channel_id_and_page do
+    it "should return the relationships" do
+      channel = FactoryGirl.create(:channel)
+      @rels1 = channel.relationships
+
+      @rels2 = Relationship.find_all_by_channel_id_and_page(channel.id, 1)
+
+      (@rels2 - @rels1).should eq([])
+    end
   end
 end
