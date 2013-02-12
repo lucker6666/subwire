dc54440c35dae28d856b27d7945645868271b7e3
benny <benny@itws.de>
2013-02-12 20:50:34 +0100

    Load comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,18 @@
 class CommentsController < ApplicationController
+  before_filter :load_message, only: [:load_all]
 
+  def load_all
+    render :partial => 'shared/comments', :locals => {
+      comments: @message.newest_comments,
+      message: @message
+    }
+  end
+
+
+  private
+
+    def load_message
+      @message = Message.find(params[:message_id])
+      authorize! :read, @message
+    end
 end
