cdecc85db0b6f5783c4be5ef1e4925bdd65a10dc
Benny <benny@itws.de>
2013-02-25 21:08:05 +0100

    Removed deprecated stuff

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -8,12 +8,4 @@ describe NotificationsController do
     # Log in first
     sign_in @rel.user
   end
-
-  it "should test fails" do
-    Notification.stub(:all_notifications_count).any_number_of_times.with(@rel.user.id).and_return(3)
-
-    get :ajax_load_all_notifications
-
-    response.should render_template(:partial => 'shared/navbar/_channelSwitcher')
-  end
 end
