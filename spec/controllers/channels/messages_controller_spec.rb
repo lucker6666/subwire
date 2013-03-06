70192325143f2fa9993f5c113fb6439b1e839fc3
phortx <benny@itws.de>
2013-03-06 22:17:41 +0100

    Made the specs green again

--- spec/controllers/channels/messages_controller_spec.rb
+++ spec/controllers/channels/messages_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 # FIXME doesn't work. need an integration test here!
 
-describe "Channels::MessagesController" do
+describe Channels::MessagesController do
   before (:each) do
     @channel = FactoryGirl.create(:channel)
     @rel = @channel.relationships.first
