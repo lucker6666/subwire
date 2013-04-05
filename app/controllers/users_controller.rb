7515390842250cede1982f8aeea3e0899d127210
phortx <benny@itws.de>
2013-04-05 18:32:04 +0200

    Improved notification system #351
    In order to improve the notitication messages and provide some additional data, the notification system had to be improved. Now a :data field may be provides for the notify_all_users method to provide up to 2 additional strings with data to display in the notification message.
    
    Additionally there's a better notification message for name changes now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,17 +46,6 @@ class UsersController < ApplicationController
 
     @user.show_login_status = params[:show_login_status]
 
-    # If the user has changed his name, notify the users to prevent identity theft
-    if params[:user][:name] != @user.name
-      @user.relationships.each do |rel|
-        Notification.notify_all_users({
-          notification_type: :username_change,
-          provokesUser: @user,
-          href: user_path(@use)
-        }, @rel.channel, current_user)
-      end
-    end
-
     # Gravatar
     if !@user.gravatar && params[:gravatar]
       @mail = @user.email
@@ -68,12 +57,27 @@ class UsersController < ApplicationController
       @user.save
     end
 
+    old_name = @user.name
+
     if @user.update_attributes(params[:user])
       if can?(:admin, :all)
         @user.is_admin = params[:is_admin]
         @user.save
       else
         sign_in(@user, bypass: true)
+      end
+
+      # If the user has changed his name, notify the users to prevent identity theft
+      if params[:user][:name] != old_name
+        @user.relationships.each do |rel|
+          Notification.notify_all_users({
+            notification_type: :username_change,
+            provokesUser: @user,
+            subject: "",
+            href: user_path(@user),
+            data: [old_name]
+          }, rel.channel, current_user)
+        end
       end
 
       feedback t('users.updated'), :success
