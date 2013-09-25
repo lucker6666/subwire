e93984c91c933f8d2d10f67fa10808f6d1dfa7fe
phortx <benny@itws.de>
2013-09-25 13:16:41 +0200

    Spec fix: avatar_path moved to helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,4 +6,31 @@ describe ApplicationHelper do
       Subwire::Application.config.backgrounds.should include(login_background)
     end
   end
+
+  describe :method do
+    describe :avatar_path do
+      before do
+        @channel = FactoryGirl.create('channel')
+        @user = @channel.relationships.first.user
+      end
+
+      context "while user has not enabled gravatar option" do
+        it "should return the avatar path for the respective user" do
+          avatar(@user).should include(@user.avatar.url)
+        end
+      end
+
+      context "while user has enabled gravatar option" do
+        it "should return gravatar path for the respective user" do
+          gravatar = Digest::MD5.hexdigest(@user.email.strip.downcase)
+          @user.gravatar = gravatar
+          @user.password = 'foobar'
+          @user.password_confirmation = 'foobar'
+          @user.save!
+
+          avatar(@user).should include('http://www.gravatar.com/avatar/' + gravatar + '?s=50')
+        end
+      end
+    end
+  end
 end
