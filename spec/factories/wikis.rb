d656b2dd8e8d1d490a901a483e43406a43bf5c55
phortx <benny@itws.de>
2013-09-30 09:31:48 +0200

    Renamed pages to wikis (spec part)

--- spec/factories/wikis.rb
+++ spec/factories/wikis.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :page do
+  factory :wiki do
     title "MyString"
     content "MyText"
     user nil
