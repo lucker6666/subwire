af08ebf4d9b9efbca4de4ebfd59b686411420029
Benjamin August <benji889@yahoo.de>
2013-10-03 18:27:01 +0000

    next small step

--- app/controllers/channels/relationships_controller.rb
+++ app/controllers/channels/relationships_controller.rb
@@ -18,7 +18,7 @@ class Channels::RelationshipsController < ApplicationController
   def new
     authorize! :create, Relationship
     @relationship = Relationship.new
-    @inviteable = '["albert", "alles", "abc"]'
+    @inviteable = Channel.can_invite_users_to_channel(current_user, current_channel)
   end
 
 
